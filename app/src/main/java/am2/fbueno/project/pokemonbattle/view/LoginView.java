bb5f3218d09b2f091b6305fbf86f1fcfdcadf8bd
maxor5991 <aljoseguba1995@gmail.com>
2016-11-18 22:30:26 -0500

    Login
    :+1:

--- app/src/main/java/am2/fbueno/project/pokemonbattle/view/LoginView.java
+++ app/src/main/java/am2/fbueno/project/pokemonbattle/view/LoginView.java
@@ -1,6 +1,8 @@
 package am2.fbueno.project.pokemonbattle.view;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 
 import am2.fbueno.project.pokemonbattle.entity.User;
 import am2.fbueno.project.pokemonbattle.utility.SecuritySession;
@@ -11,6 +13,21 @@ import am2.fbueno.project.pokemonbattle.utility.SecuritySession;
 
 public abstract class LoginView extends View {
     protected Class<?> mainClass;
+
+    public void PopUp(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Chupamela :3")
+                .setTitle("Holi Boli")
+                .setCancelable(false)
+                .setNeutralButton("Aceptar",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
 
     public LoginView(Class<?> mainClass) {
         this.mainClass = mainClass;
